7ed9d643c55d04204454dd063a0153d8b3febdfa
DD~! <devddstuff@gmail.com>
2016-03-17 17:08:36 -0400

    Refreshed random lines

--- README.md
+++ README.md
@@ -3,11 +3,21 @@
 
 ![Clippy](http://didi.wcantin.ca/p/clippy2.png)
 
-While FuckingClippy is Mono compatible, it still needs work.
+---
+
+### About
+FuckingClippy is a reincarnation of the long lost Office assistant.
 
 The Wiki is available [here][wiki] and releases can be found [here][releases].
 
 Using the GNU General Public License, version 3 license (GPL-3.0) which you may read [here][license].
+
+---
+
+### Notes
+While FuckingClippy is Mono compatible, it still needs heavy improvement.
+
+---
 
 Copyright © Microsoft 1997-2003 for their Clippit Office Assistant, mostly known as Clippy.
 
--- Source/Character.cs
+++ Source/Character.cs
@@ -93,22 +93,34 @@ namespace FuckingClippy
 
         internal static void SayRandom()
         {
-            string[] str =
+            string[] s =
             {
-                "I heard you like pies.",
+                "So, you come here often?",
                 "Would you like help with hugging yourself?",
-                "I got you a gift for christmas. It's called a kernel panic! Let me show you!",
-                "test123 guys can you hear me",
-                "foo dick",
-                "Looks like you're trying to focus, need some help?",
-                "I hope you're enjoying staring at my body.",
-                "WHY DOESN'T EMOJIS WORK HUH",
-                "I'm bored.",
-                "DUDE WHERE'S MY GAC? >:-(",
-                "Meow."
+                "(this isFor ThE fAnS and G GaMerGirls)",
+                "Welcome. Welcome to City 17.",
+                "I can see you, but can you see me?",
+                "Do you need help looking at that screen?",
+                "I know, I'm not as fun as GonzoBuddy, but at least I'm not spyware, right?",
+                "It would be a shame if something happened to these fil-- OOOPSS!",
+                "[MAXIMUM ARMOR]",
+                "Are you sure you want to click that?",
+                "0x4E4F4246\nDid I spook you?",
+                "Did you know that Intel's first microprocessor, the Intel 4004, was released in 1971, had 2,300 transistors measuring 10 microns?",
+                "I am not an AI, just a bunch of IL instructions.",
+                "Seems like you need help living your life there buddy.",
+                "The program '[3440] FuckingClippy.vshost.exe' has exited with code 0 (0x0).",
+                "<3?",
+                "Did you know that I'm a vegan?",
+                "SUFFER();",
+                "rawrrr x33",
+                "I still have transparency and form autosizing issues on Mono!",
+                "Deleting your files...",
+                "S-sorry, senpai..",
+                "Hey do you mind if I use more memory?",
             };
 
-            Say(str[new Random().Next(0, str.Length)]);
+            Say(s[new Random().Next(0, s.Length)]);
         }
 
         static Control[] GetSay(string pText)
